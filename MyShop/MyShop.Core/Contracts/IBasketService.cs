390bddfe4afeeb434488f06edfa710bdf1f7897e
Saad Masood <smasood@catchpoint.com>
2019-07-08 17:12:05 -0400

    create BasketItemViewModel, IBasketService

---
+++ MyShop/MyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+    }
+}

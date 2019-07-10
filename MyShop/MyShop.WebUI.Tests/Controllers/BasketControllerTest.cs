af0b63a3f2cc36b38183e39b26cfdfac116d89bf
Saad Masood <smasood@catchpoint.com>
2019-07-10 17:46:41 -0400

    add basket item - unit test

---
+++ MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.WebUI.Tests.Mocks;
+using MyShop.Services;
+using System.Linq;
+using MyShop.WebUI.Controllers;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTest
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            // Setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+            
+            // Act
+            //basketService.AddToBasket(httpContext, "1"); // use this to test service instead of controller
+            controller.AddToBasket("1");
+
+            // Assert
+            Basket basket = baskets.Collection().FirstOrDefault();
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+        }
+    }
+}
